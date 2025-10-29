77f438e5568783a17646b0d2a279a0e121c82334
Нуржан Жумабек <242260@astanait.edu.kz>
2025-10-29 15:05:13 +0500

    Delete SMSNotification.java

--- SMSNotification.java
+++
@@ -1,6 +0,0 @@
-public class SMSNotification implements NotificationStrategy {
-    @Override
-    public void send(String message) {
-        System.out.println("Sending SMS: " + message);
-    }
-}
