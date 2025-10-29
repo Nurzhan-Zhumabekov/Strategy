61bf5cd975b1945d76639a5172e77c4fccffbd85
Нуржан Жумабек <242260@astanait.edu.kz>
2025-10-29 15:04:23 +0500

    Delete EmailNotification.java

--- EmailNotification.java
+++
@@ -1,8 +0,0 @@
-public class EmailNotification implements NotificationStrategy {
-    @Override
-    public void send(String message) {
-        System.out.println("Sending EMAIL: " + message);
-    }
-}
-
-
